1831308f571cbc89cf7020daa9ea36322585f097
halit <hltinan@gmail.com>
2018-03-07 18:40:22 +0300

    Added AlertController for image and video

--- Chat App/ViewController.swift
+++ Chat App/ViewController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 import JSQMessagesViewController
+import MobileCoreServices
+import AVKit
 
-class ViewController: JSQMessagesViewController {
-
+class ViewController: JSQMessagesViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    var imagePicker = UIImagePickerController()
     var messages = [JSQMessage]()
     override func viewDidLoad() {
         super.viewDidLoad()
         senderId = "1"
         senderDisplayName = "hlt"
         
+
         
         // attach hidden
-        inputToolbar.contentView.leftBarButtonItem = nil
+//        inputToolbar.contentView.leftBarButtonItem = nil
     }
 
     override func didReceiveMemoryWarning() {
@@ -64,11 +67,78 @@ class ViewController: JSQMessagesViewController {
         return messages[indexPath.item].senderId == senderId ? 0 : 20
     }
     
+    // didPress SEND BUTTON
     override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
-        let ref = Constants.dbRef.childByAutoId()  // Uniqe id
-        let message = ["senderId" : senderId , "sendername" : senderDisplayName , "message" : text]
-        ref.setValue(message)
+//        let ref = Constants.dbRef.childByAutoId()  // Uniqe id
+//        let message = ["senderId" : senderId , "sendername" : senderDisplayName , "message" : text]
+//        ref.setValue(message)
+        
+        self.messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text))
+        collectionView.reloadData()
         finishSendingMessage()
     }
+    
+    override func didPressAccessoryButton(_ sender: UIButton!) {
+        let actionSheet = UIAlertController(title: "Image", message: "Please Select Image", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        let image = UIAlertAction(title: "Images", style: UIAlertActionStyle.default) { (action) in
+            
+            selectSourceType(type: kUTTypeImage)
+            
+        }
+        
+        let camera = UIAlertAction(title: "Camera", style: UIAlertActionStyle.default) { (action) in
+            
+            selectSourceType(type: kUTTypeMovie)
+            
+        }
+        let video = UIAlertAction(title: "Video", style: .default) { (action) in
+            self.imagePicker.delegate = self
+            
+        }
+        
+        func selectSourceType(type : NSString){
+            self.imagePicker.delegate = self
+            self.imagePicker.mediaTypes = [type as String]
+            self.present(self.imagePicker, animated: true, completion: nil)
+            
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+        
+        actionSheet.addAction(image)
+        actionSheet.addAction(camera)
+        actionSheet.addAction(cancel)
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    // Image Selected Settings
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            let JSQImage = JSQPhotoMediaItem(image : selectedImage)
+            self.messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, media: JSQImage))
+        }else if let selectedVideo = info[UIImagePickerControllerMediaURL] as? URL {
+            let JSQVideo = JSQVideoMediaItem(fileURL: selectedVideo, isReadyToPlay: true)
+            self.messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, media: JSQVideo))
+        }
+        dismiss(animated: true, completion: nil)
+        collectionView.reloadData()
+    }
+    
+    //Play Video
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, didTapMessageBubbleAt indexPath: IndexPath!) {
+        let message = messages[indexPath.item]
+        if message.isMediaMessage{
+            if let videoMessage = message.media as? JSQVideoMediaItem {
+                let player = AVPlayer(url: videoMessage.fileURL)
+                let playerController = AVPlayerViewController()
+                playerController.player = player
+                present(playerController, animated: true, completion: nil)
+                
+            }
+        }
+    }
+    
 }
 
